69f3a2ceaf48fc6f9dfd844fdf85dcfd9e50e65b
Takehiro <tsunmike@mc.pikara.ne.jp>
2025-09-06 16:28:04 +0900

    1.02

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',  // これが明示的な相対パスの設定
+    base: './',  // 明示的な相対パスの設定
+    build: {
+        outDir: 'docs'  // ← 出力先をdocsに変更
+    }
 });
